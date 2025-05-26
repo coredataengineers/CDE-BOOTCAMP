b71542fd4810d98b2d770a9bf3a4093060bb4d41
cappymayor <mayowaakinyele40@gmail.com>
2025-05-26 13:32:15 +0200

    restructure terraform

--- 10_terraform/standalone_resources/10_dms/3_s3_endpoint.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_dms_s3_endpoint" "example" {
-  endpoint_id             = "donnedtipi"
-  endpoint_type           = "target"
-  bucket_name             = "beckut_name"
-  service_access_role_arn = aws_iam_role.example.arn
-
-  depends_on = [aws_iam_role_policy.example]
-}
--- 10_terraform/standalone_resources/1_iam/3_iam_access_key.tf
+++
@@ -1,5 +0,0 @@
-
-resource "aws_iam_access_key" "lb" {
-  user    = aws_iam_user.lb.name
-  pgp_key = "keybase:some_person_that_exists"
-}
